7b8b87a4f4caac86d9c582b36887917ae49c1bb7
Taiwo <tayeabidakun@gmail.com>
2021-09-01 02:02:52 +0100

    formik and yup codes added

--- src/App.js
+++ src/App.js
@@ -1,84 +1,92 @@
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Route, Switch } from 'react-router';
-import Admin from './components/newclass/inventory/Admin';
-// import { Redirect } from 'react-router-dom';
+import NewFunc from './components/newclass/NewFunc';
+import ReduxClass from './components/newclass/ReduxClass';
+import ReduxFunc from './components/newclass/ReduxFunc';
+import SecondChild from './components/newclass/secondChild';
+import ChildApp from './components/newclass/TestChild';
+// import { editName, increment, decrement } from './components/newclass/myredux/actions';
 
-// import About from './components/newclass/About';
-// import Contact from './components/newclass/Contact';
-// import FirstContact from './components/newclass/FirstContact';
+let details = {firstname: "Taiwo", lastname: "", password: "", email: ""}
+const submitMyForm = values=>{
+  console.log(values);
+}
+const myValidation = Yup.object().shape({
+  firstname: Yup.string().min(6, "Firstname is too short").required("You need to fill lastname"),
+  lastname: Yup.string().min(6, "lastname is too short").required("You need to fill lastname"),
+  password: Yup.string().min(8, "password is too short").required(),
+  email: Yup.string().email("Type a valid email address").required(),
+})
 
-
-// import Home from './components/newclass/Home';
-// import NavBar from './components/newclass/NavBar';
-// import NotFoundComponent from './components/newclass/NotFoundComponent';
-// import User from './components/newclass/User';
-// const App = ()=>{
-//   const [name, setName] = useState("Taiwo");
-
-//   return(
-//      <>
-
-//    <NavBar/>
-
-//    <button onClick={()=>setName("Kenny")}>change name</button>
-//      <Switch>
-     
-//      <Route path="/" exact  component={Home} />
-//      <Route path="/dashboard/:myName" component={User}/>
-     
-//         <Route path="/info" component={About}/>
-//         <Route path="/contact" exact component={Contact}/>
-//         <Route path="/contact/first" component={FirstContact}/>
-//         <Route component={NotFoundComponent}/>
-//      </Switch>
-  
-//   </>
-//   )
-// }
-// export default App;
-// import React from 'react';
-import Landing from './components/newclass/inventory/landing';
-import Nav from './components/newclass/inventory/Nav';
-import User from './components/newclass/inventory/User';
-// import EffectClass from './components/newclass/EffectClass';
-// import EffectFunction from './components/newclass/EffectFunction';
 const App = ()=>{
-  const [goods, setGoods] = useState([]);
-  const deleteGood = i=>{
-    let newGoods = goods.filter((_, ind)=>i!==ind);
-    setGoods(newGoods);
- }
- const addGood = good=>{
-  setGoods([...goods, good]);
-}
-const deductGood = goodsToDeduct =>{
-  let getAllGoods = [...goods];
-  for (let index = 0; index < getAllGoods.length; index++) {
-    for (let i = 0; i < goodsToDeduct.length; i++) {
-      if(index === goodsToDeduct[i].index && getAllGoods[index].goodQty>= goodsToDeduct[i].qty){
-        getAllGoods[index].goodQty -=goodsToDeduct[i].qty; 
-      }
-      
-    }
-    
-  }
-  setGoods(getAllGoods);
-}
-// const [dept, setDept] = useState();
-
+  
+  // let myVal = useSelector(state=>state.num);
+  // let myName = useSelector(state=>state.name);
+  // let dispatch = useDispatch();
+let [login, setLogin] = useState(false);
+let userDetails =  [
+{
+  type: "text", 
+  val: "firstname"
+}, 
+{
+type: "text",
+val: "lastname"
+},
+{
+  type: "password",
+  val: "password"
+},
+{
+  type: "email",
+  val: "email"
+}]
+  
   return(
 
     <>
-    <Nav/>
-    <br/>
-    <Switch>
-      <Route path="/" exact component={Landing}/>
-      <Route path="/admin" render={(props)=><Admin {...props} goods={goods} deleteGood={deleteGood} addGood={addGood}/>}/>
-      <Route path="/user" render={(props)=><User {...props} goods={goods} deductGood={deductGood}/>}/>
-    </Switch>
+    <Formik
+    initialValues={details}
+    onSubmit={submitMyForm}
+    validationSchema={myValidation}
 
-  
-      
+    
+    >
+     {()=>(
+      <Form>
+        {userDetails.map((detail, i)=>(
+          <div key={i}>
+            <Field name={detail.val} type={detail.type} placeholder={detail.val}/>
+            <ErrorMessage name={detail.val}/>
+          </div>
+        ))}
+        <button>submit form</button>
+        {/* <Field name="firstname" placeholder="firstname"/>
+        <ErrorMessage name="firstname"/>
+        <Field name="lastname" placeholder="lastname"/>
+        <ErrorMessage name="lastname"/>
+        <Field name="email" placeholder="email"/>
+        <ErrorMessage name="email"/>
+        <Field type="password" name="password"/>
+        <ErrorMessage name="password"/> */}
+        {/* <button>submit form</button> */}
+      </Form>
+     )}
+
+    </Formik>
+    {/* Hello
+    <button onClick={()=>setLogin(true)}>login</button>
+    <button onClick={()=>setLogin(false)}>logout</button>
+    <ChildApp loginStatus={login}/>
+<SecondChild loginStatus={login}/>
+   */}
+    {/* <ReduxFunc/>
+<NewFunc/> */}
+    {/* <ReduxClass/> */}
+   
     </>
   )
 }
